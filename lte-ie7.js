b1cbc8269d25a4d6958e8601b57a1a8badd1b923
Ksagredo <srta.keyla.sagredo@gmail.com>
2013-10-14 09:14:19 -0300

    Estructura semantica Index y CSS.
    Estructura Semantica Index.
    Estilos en CSS Header, y NAV con tipografias vectoriales.

---
+++ lte-ie7.js
@@ -0,0 +1,34 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-home' : '&#xe000;',
+			'icon-envelope' : '&#xe001;',
+			'icon-linkedin' : '&#xe002;',
+			'icon-facebook' : '&#xe003;',
+			'icon-twitter' : '&#xe004;',
+			'icon-users' : '&#xe005;',
+			'icon-cart' : '&#xe006;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
